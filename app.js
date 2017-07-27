18f35473921ec40b4cf682d930b1131c54972b3c
Alex Pan <alexpan2002@msn.com>
2017-07-27 14:11:18 +0800

    update

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-console.log("req", req);
+// console.log("req", req);
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
